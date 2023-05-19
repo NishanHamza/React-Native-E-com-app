e4cfefc0f0ff93f13434f5f7fbf50ce9df22dabf
NishanHamza <nishanhamza2@gmail.com>
2023-05-19 21:10:40 +0600

    upgrade react-native

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import Main from './Main';
+
+export default function App() {
+  return (
+    <Main />
+  );
+}
---
+++ Main.jsx
@@ -0,0 +1,20 @@
+import { View, Text, SafeAreaView, StatusBar, Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./screens/Home";
+
+const Stack = createNativeStackNavigator();
+
+const Main = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Group>
+          <Stack.Screen name="home" component={Home} />
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Main;
